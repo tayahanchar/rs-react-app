c03a2cff93275ad2de40a2aeed4d4c4f3ac8bfed
Taya Hanchar <tayahanchar@gmail.com>
2025-07-15 22:59:40 +0300

    feat: add components and main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,100 @@
-import { useState } from 'react';
+import { Component } from 'react';
 import './App.css';
+import Header from './components/header/Header';
+import fetchData from './utils/fetchData';
+import Main from './components/main/Main';
+import fetchPokemons from './utils/fetchPokemonsWithDescription';
 
-function App() {
-  const [count, setCount] = useState(0);
+interface IPokemon {
+  name: string;
+  url: string;
+}
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+class App extends Component {
+  state = {
+    pokemons: [],
+    isError: false,
+    isLoading: true,
+  };
+
+  async componentDidMount() {
+    const searchStr = localStorage.getItem('searchStr');
+
+    if (searchStr) {
+      this.handleSearch(searchStr);
+    } else {
+      this.setState({ isLoading: true });
+      try {
+        const pokemons = await fetchData('https://pokeapi.co/api/v2/pokemon');
+        const pokemonsWithDescription = await fetchPokemons(pokemons.results);
+
+        this.setState({
+          pokemons: pokemonsWithDescription,
+          isError: false,
+          isLoading: false,
+        });
+      } catch (error) {
+        console.log(error);
+        this.setState({ isError: true, isLoading: false });
+      }
+    }
+  }
+
+  handleSearch = async (str: string) => {
+    localStorage.setItem('searchStr', str);
+
+    this.setState({ isLoading: true });
+
+    try {
+      if (str.trim().length === 0) {
+        this.setState({ isLoading: true });
+        try {
+          const pokemons = await fetchData('https://pokeapi.co/api/v2/pokemon');
+          const pokemonsWithDescription = await fetchPokemons(pokemons.results);
+
+          this.setState({
+            pokemons: pokemonsWithDescription,
+            isError: false,
+            isLoading: false,
+          });
+        } catch (error) {
+          console.log(error);
+          this.setState({ isError: true, isLoading: false });
+        }
+      }
+      const result = await fetchData(
+        `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`
+      );
+
+      const filtredData = result.results.filter((pokemon: IPokemon) =>
+        pokemon.name.startsWith(str.toLowerCase())
+      );
+
+      const pokemonsWithDescription = await fetchPokemons(filtredData);
+
+      this.setState({
+        pokemons: pokemonsWithDescription,
+        isError: false,
+        isLoading: false,
+      });
+    } catch (error) {
+      console.log(error);
+      this.setState({ isError: true, isLoading: false });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Header handleSearch={this.handleSearch} />
+        <Main
+          pokemonsList={this.state.pokemons}
+          isError={this.state.isError}
+          isLoading={this.state.isLoading}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    );
+  }
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import ErrorBoundary from './components/errorBoundary/ErrorBoundary.tsx';
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.getElementById('root')!).render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+);
